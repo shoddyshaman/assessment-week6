a1cf74d3851e3b90b13bc8faa182c76e713e7e57
jltrent12 <jltrent12@gmail.com>
2021-10-08 12:02:42 -0500

    Adding assessment week-6 responses: Part 1,2,3

--- qa/tictactoe.test.ts
+++ qa/tictactoe.test.ts
@@ -18,3 +18,44 @@ test('I can start a game', async () => {
     await button.click();
     
 });
+
+// Check that clicking the upper left square adds an X to the square.
+test('clicking upper left adds an X', async () => {
+    let upperLeft = await driver.findElement(By.id('cell-0'))
+    await upperLeft.click()
+
+    await driver.sleep(3000)
+    expect(await upperLeft.getText()).toContain('X')
+    await driver.sleep(3000)
+})
+
+// Check that clicking the upper right square adds an X to the square.
+test('clicking upper right adds an X', async () => {
+    let upperRight = await driver.findElement(By.id('cell-2'))
+    await upperRight.click()
+
+    await driver.sleep(3000)
+    expect(await upperRight.getText()).toContain('X')
+    await driver.sleep(3000)
+})
+
+// Check that clicking the lower right square adds an X to the square.
+test('clicking the lower right adds an X', async () => {
+    let lowerRight = await driver.findElement(By.id('cell-8'))
+    await lowerRight.click()
+
+    await driver.sleep(3000)
+    expect(await lowerRight.getText()).toContain('X')
+    await driver.sleep(3000)
+})
+
+// [Harder] Check to see that the computer moves (adds an O) to a square after clicking on a square.
+test('computer moves (adds an O) after click', async () => {
+    let humanMove = await driver.findElement(By.css('tbody'))
+    await humanMove.click()
+    await driver.sleep(2000)
+    let computerMove = await driver.findElement(By.css('tbody')).getText()
+
+    expect(computerMove).toContain('O')
+    await driver.sleep(3000)
+})
