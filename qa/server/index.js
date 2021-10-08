756f4ac8504956fcbb78278d10a2ebd603cb98fd
jltrent12 <jltrent12@gmail.com>
2021-10-08 15:22:11 -0500

    Adding styling and some fixes

--- qa/server/index.js
+++ qa/server/index.js
@@ -7,6 +7,10 @@ app.get('/',function(req,res) {
   res.sendFile(path.join(__dirname, '../tictacjs.html'));
 });
 
+app.get('/styles.css',function(req,res) {
+  res.sendFile(path.join(__dirname, '../styles.css'));
+});
+
 const port = process.env.PORT || 4000
 
 app.listen(port, () => {
